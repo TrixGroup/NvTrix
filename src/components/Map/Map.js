1e6505abc342d64e98735dc80adc0c843bf13238
Tomdieu <ivantomdio@gmail.com>
2022-09-23 06:11:03 +0100

    updated

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -48,15 +48,21 @@ const CustomMap = (props) => {
 
             function success({coords}){
                 console.log({coords});
-                if(map.current){
-                    map.current.flyTo({
-                        center:[coords.longitude,coords.latitude],
-                        essential: true,
-                        speed: 0.2,
-                        zoom:9
-                    })
-                    map.current.setCenter([coords.longitude,coords.latitude]);
-
+                if(map.current ){
+                    console.log(coords.longitude);
+                    map.current.on('load',()=>{
+                        if(map.current.isStyleLoaded()){
+                            map.current.flyTo({
+                                center:[coords.longitude,coords.latitude],
+                                essential: true,
+                                speed: 0.2,
+                                zoom:13
+                            });
+                            map.current.setZoom(10);
+                            map.current.setCenter([coords.longitude,coords.latitude]);    
+                        }
+                        
+                    });
                 }
             }
 
