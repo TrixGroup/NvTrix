5bfb0e32630c67b790eb254d08f8764a967ba35f
Tomdieu <ivantomdio@gmail.com>
2022-09-23 08:34:33 +0100

    updated

--- src/Service/mapStyle.js
+++ src/Service/mapStyle.js
@@ -1,6 +1,6 @@
 
 export const mapStyle = {
-	'navigation_dark':'mapbox://styles/ivantom/cl8bukr96001x14pqpxm56iz5',
+	'navigation_dark':'mapbox://styles/ivantom/cl8da4zaz00af14qq0bqf9rj1',
 	'navigation_light':'mapbox://styles/ivantom/cl8bv5wti000q15rt7d7x3wmc',
 	'city':'mapbox://styles/mapbox/streets-v11',
 	satalite:'',
